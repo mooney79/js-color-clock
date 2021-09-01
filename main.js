cba8b6f770092ac3fb49626b3cc8d552cc4a88cf
Samuel Mooney <lappletop@Samuels-MacBook-Pro.local>
2021-09-01 13:46:35 -0400

    initial clock logging implemented

---
+++ main.js
@@ -0,0 +1,36 @@
+/*
+////////////////////////////////////////////
+///////     LIST OF OBJECTIVES       ///////
+///////////////////////////////////////////
+
+* console.log the current time on page load
+* console.log the current time every second
+* Display the current time every second
+* Display the current time, padded with zeros to be exactly two digits long, every second
+* console.log the percentage of a minute that the current seconds represents (e.g., if 30 seconds have elapsed, console.log 0.5)
+* Using the percent above, dynamically modify the length of the timer bar
+* console.log a hexidecimal color that is based on the current second, every second
+* Dynamically update the background color of the page using the hexidecimal color
+* Display the generated color's hex value on hover
+* Mission Accomplished!
+
+*/
+
+// function initializeTimer(){  //go into body tag, add onload="initializeTimer()" <-- Can I do this with JS?
+//     console.log();
+// };
+
+function clock() {
+    var timeRaw = new Date();
+    var hrs = timeRaw.getHours();
+    var mins = timeRaw.getMinutes();
+    var secs = timeRaw.getSeconds();
+    var timeString =`${hrs}:${mins}:${secs}`;
+    return timeString
+  }
+
+function logClock() {
+    console.log(clock());
+}
+
+logClock();
